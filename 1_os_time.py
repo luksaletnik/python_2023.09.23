1a1ec252c8eff3125ff038a09f8281da5bbab089
Łukasz Saletnik <luksaletnik@gmail.com>
2023-09-26 11:39:11 +0200

    v1.0.0

--- 1_os_time.py
+++ 1_os_time.py
@@ -1,7 +1,7 @@
 import os
 
 print('I am on the path:', os.getcwd())
-os.chdir('C:\\Users\\luksa\\PycharmProjects\\pythonProject_2.1')
+os.chdir('C:\\Users\\luksa\\PycharmProjects\\pythonProject_2\\python_2023.09.23\\pythonProject_2.1')
 # os.chdir(r'C:\Users\luksa\PycharmProjects\pythonProject_2.1')       # r - raw string, alternatywa do powyższego
 print('New path:', os.getcwd())
 
