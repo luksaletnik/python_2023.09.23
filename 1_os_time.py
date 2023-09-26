04e34a33c666994c6e441e71fa51c6614e37efe2
Łukasz Saletnik <luksaletnik@gmail.com>
2023-09-26 16:00:31 +0200

    Updated

--- 1_os_time.py
+++ 1_os_time.py
@@ -1,8 +1,22 @@
 import os
+import time
 
 print('I am on the path:', os.getcwd())
 os.chdir('C:\\Users\\luksa\\PycharmProjects\\pythonProject_2\\python_2023.09.23\\pythonProject_2.1')
 # os.chdir(r'C:\Users\luksa\PycharmProjects\pythonProject_2.1')       # r - raw string, alternatywa do powyższego
 print('New path:', os.getcwd())
 
+# Funkcje py na: obliczeniowo.pl
+
 os.mkdir('new_folder')
+time.sleep(2)
+os.renames('new_folder', 'newer_folder')
+time.sleep(2)
+os.rmdir('newer_folder')
+time.sleep(2)
+
+print('Done')
+
+# >> - przekierowanie do strumienia
+
+os.system('cmd /c "cd C:\\Users\\luksa\\PycharmProjects\\pythonProject_2\\python_2023.09.23\\pythonProject_2.1 && dir /s new.txt >> result.txt"')
